1e7ebd7f2faeac8546264d97490e20105a08293f
helghan1 <kbaumkb92@gmail.com>
2026-03-27 17:34:13 +0100

    Fehlerbehebung

--- infografik/vite.config.js
+++ infografik/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "./", // WICHTIG: Erlaubt das Laden von überall
+  build: {
+    rollupOptions: {
+      output: {
+        // Feste Namen statt kryptischer Codes
+        entryFileNames: `assets/prozess-grafik.js`,
+        chunkFileNames: `assets/prozess-grafik.js`,
+        assetFileNames: `assets/prozess-grafik.[ext]`,
+      },
+    },
+  },
+});
